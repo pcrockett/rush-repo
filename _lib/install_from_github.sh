05cc80de2fab79e41d6dc881e5fa7f3048d09223
Philip Crockett <contact@philcrockett.com>
2024-01-21 11:25:42 -0600

    install_from_github util and restic package

---
+++ _lib/install_from_github.sh
@@ -0,0 +1,43 @@
+# shellcheck shell=bash
+
+install_from_github() {
+    GITHUB_LATEST_TAG="$(github_latest_tag "${GITHUB_ORG}" "${GITHUB_REPO}")"
+    export GITHUB_LATEST_TAG
+
+    local __installed_version \
+        __download_url \
+        __artifact_name
+
+    GITHUB_LATEST_VERSION="$(latest_version)"
+    export GITHUB_LATEST_VERSION
+
+    __installed_version="$(installed_version)"
+
+    if [ "${GITHUB_LATEST_VERSION}" = "${__installed_version}" ]; then
+        log_attention "Already installed and up-to-date."
+        exit 0
+    fi
+
+    __download_url="$(download_url)"
+    __artifact_name="$(artifact_name)"
+
+    temp_dir="$(mktemp --directory --tmpdir "pcrockett_gh_install.XXXX")"
+    cleanup() {
+        local exit_code="${?}"
+        popd &> /dev/null || panic "unable to cd away from ${temp_dir}"
+        rm -rf "${temp_dir}"
+        return "${exit_code}"
+    }
+    pushd "${temp_dir}" &> /dev/null || cleanup
+
+    log_info "Download ${__download_url} to ${temp_dir}/${__artifact_name}..."
+    curl_download "${__download_url}" > "${temp_dir}/${__artifact_name}" || cleanup
+    chmod +x "${temp_dir}/${__artifact_name}" || cleanup
+
+    log_info "Installing..."
+    ARTIFACT_PATH="${temp_dir}/${__artifact_name}" \
+        install_artifact || cleanup
+
+    log_attention "Done."
+    cleanup
+}
--- _lib/vars.sh
+++ _lib/vars.sh
@@ -2,3 +2,6 @@
 
 RUSH_USER_BIN="${RUSH_USER_BIN:-"${HOME}/.local/bin"}"
 RUSH_GLOBAL_BIN="${RUSH_GLOBAL_BIN:-"/usr/local/bin"}"
+
+RUSH_PACKAGE_NAME="$(basename "${PWD}")"
+export RUSH_PACKAGE_NAME
