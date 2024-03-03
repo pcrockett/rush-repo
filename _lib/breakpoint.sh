c3eebda9db301b30a0068ec417e9a5c8f8a69d25
Philip Crockett <contact@philcrockett.com>
2024-03-03 12:34:51 +0100

    breakpoint debugging tool

---
+++ _lib/breakpoint.sh
@@ -0,0 +1,23 @@
+# shellcheck shell=bash
+
+breakpoint() {
+    # enable a debug prompt that allows you to see expanded variables and execute each line
+    # thereafter step-by-step
+
+    debug_prompt() {
+        # ex usage:
+        #
+        #     debug_prompt filename line_num command_being_executed
+        #
+        # outputs:
+        #
+        #     [filename:line_num] command_being_executed
+        #
+        # ...and waits for the user to press enter
+
+        # shellcheck disable=SC2317  # unreachable code false positive
+        (read -r -p "[$1:$2] $3")
+    }
+    set -x
+    trap 'debug_prompt "${BASH_SOURCE[0]}" ${LINENO} "${BASH_COMMAND}"' DEBUG
+}
