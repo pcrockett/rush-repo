cf5aed2666ef661296958ceced54e11eb326ba67
Philip Crockett <contact@philcrockett.com>
2024-08-15 20:23:18 +0200

    only call sudo if you need it

--- _lib/install_global_bin.sh
+++ _lib/install_global_bin.sh
@@ -5,5 +5,11 @@ install_global_bin() {
 
     local source_bin_file="${1}"
     test -d "${RUSH_GLOBAL_BIN}" || panic "Not a directory: ${RUSH_GLOBAL_BIN}"
-    sudo install "${source_bin_file}" "${RUSH_GLOBAL_BIN}"
+
+    command=(install "${source_bin_file}" "${RUSH_GLOBAL_BIN}")
+    if [ "$(id --user)" -eq 0 ]; then
+        "${command[@]}"
+    else
+        sudo "${command[@]}"
+    fi
 }
