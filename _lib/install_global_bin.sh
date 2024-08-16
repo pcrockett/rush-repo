1e52e8ace6b1ec1b226f011ede89ee8266618264
Philip Crockett <contact@philcrockett.com>
2024-08-16 18:58:06 +0200

    refactor all `sudo` calls to new `as_root` function

--- _lib/install_global_bin.sh
+++ _lib/install_global_bin.sh
@@ -1,15 +1,7 @@
 # shellcheck shell=bash
 
 install_global_bin() {
-    require_commands sudo
-
     local source_bin_file="${1}"
     test -d "${RUSH_GLOBAL_BIN}" || panic "Not a directory: ${RUSH_GLOBAL_BIN}"
-
-    command=(install "${source_bin_file}" "${RUSH_GLOBAL_BIN}")
-    if [ "$(id --user)" -eq 0 ]; then
-        "${command[@]}"
-    else
-        sudo "${command[@]}"
-    fi
+    as_root install "${source_bin_file}" "${RUSH_GLOBAL_BIN}"
 }
