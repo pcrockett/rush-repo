4b4b1ae07d3f88650fc62be73d0e56c8de65dd79
Philip Crockett <contact@philcrockett.com>
2024-10-03 08:15:04 +0200

    refactor file hashes, move fzfdump to global bin directory

---
+++ _lib/get_hash.sh
@@ -0,0 +1,17 @@
+# shellcheck shell=bash
+
+get_hash() {
+    # example usage: `get_hash < some/file/path`
+    sha256sum - | cut --fields 1 --delimiter " " --only-delimited
+}
+
+get_file_hash() {
+    # example usage: `get_file_hash some/file/path`
+    local path="${1}"
+    test -f "${path}" || return 0 # Return nothing
+    get_hash < "${path}"
+}
+
+files_are_same() {
+    test "$(get_file_hash "${1}")" == "$(get_file_hash "${2}")"
+}
