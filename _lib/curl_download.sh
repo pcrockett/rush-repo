901776149147676ae8d1df90c988d9461a5a7890
Phil Crockett <contact@philcrockett.com>
2025-04-02 21:27:14 +0200

    allow authenticated github requests

--- _lib/curl_download.sh
+++ _lib/curl_download.sh
@@ -3,9 +3,19 @@
 curl_download() {
     local url="${1}"
     require_commands curl
-    curl --proto '=https' --tlsv1.2 \
-        --silent \
-        --show-error \
-        --fail \
-        --location "${url}"
+
+    curl_cmd=(
+        curl
+        --proto '=https'
+        --tlsv1.2
+        --silent
+        --show-error
+        --fail
+    )
+
+    if [ "${GITHUB_TOKEN:-}" != "" ] && [[ "${url}" =~ ^https://api\.github\.com/ ]]; then
+        curl_cmd+=(--header "Authorization: Bearer ${GITHUB_TOKEN}")
+    fi
+
+    "${curl_cmd[@]}" --location "${url}"
 }
