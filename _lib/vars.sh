b449c8fee8cb1a5a0800a0ac37c1eff525ec308f
Philip Crockett <philip.crockett@thermondo.de>
2025-06-18 15:19:00 +0200

    feat(postman): desktop file and scheme handler

--- _lib/vars.sh
+++ _lib/vars.sh
@@ -1,9 +1,9 @@
 # shellcheck shell=bash
 
-XDG_STATE_HOME="${XDG_STATE_HOME:-"${HOME}/.local/state"}"
-XDG_DATA_HOME="${XDG_DATA_HOME:-"${HOME}/.local/share"}"
-XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
-XDG_CACHE_HOME="${XDG_CACHE_HOME:-"${HOME}/.cache"}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-"${HOME}/.local/state"}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-"${HOME}/.local/share"}"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-"${HOME}/.config"}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-"${HOME}/.cache"}"
 
 RUSH_GLOBAL_BIN="${RUSH_GLOBAL_BIN:-"/usr/local/bin"}"
 
