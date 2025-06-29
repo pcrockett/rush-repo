9cca41c5312e6e80689a8abbf9b3ad07ff74204d
Phil Crockett <contact@philcrockett.com>
2025-06-29 08:53:55 +0200

    chore(lib): extract tar utils

---
+++ _lib/extract_tar.sh
@@ -0,0 +1,39 @@
+# shellcheck shell=bash
+
+extract_tar_gz_url() {
+    local src_url="${1}"
+    shift
+    local dest="${1}"
+    shift
+    local tar_args=("$@")
+    local temp_dir
+    temp_dir="$(mktemp_dir)"
+    cleanup() {
+        local exit_code=$?
+        rm -rf "${temp_dir}"
+        return $exit_code
+    }
+
+    {
+        curl_download "${src_url}" > "${temp_dir}/archive"
+        extract_tar_gz "${temp_dir}/archive" "${dest}" "${tar_args[@]}"
+    } || cleanup
+    cleanup
+}
+
+extract_tar_gz() {
+    local archive_path="${1}"
+    shift
+    local dst_path="${1}"
+    shift
+    local tar_args=("$@")
+
+    rm -rf "${dst_path}.new"
+    mkdir --parent "${dst_path}.new"
+    tar --directory "${dst_path}.new" -xzf "${archive_path}" "${tar_args[@]}"
+
+    rm -rf "${dst_path}.old"
+    test ! -d "${dst_path}" || mv "${dst_path}" "${dst_path}.old"
+
+    mv "${dst_path}.new" "${dst_path}"
+}
