763d0a063440df0a2a5f5f06c08c189f0a0ebc61
Philip Crockett <philip.crockett@thermondo.de>
2025-08-27 09:16:50 +0200

    feat: wtfutil

--- _lib/install_success.sh
+++ _lib/install_success.sh
@@ -2,7 +2,12 @@
 
 install_success() {
     local installed_packages_dir
+    local version="${1:-}"
     installed_packages_dir="$(repo_state_dir)/installed_packages"
     mkdir --parent "${installed_packages_dir}"
-    touch "${installed_packages_dir}/${RUSH_PACKAGE_NAME}"
+    if [ "${version}" == "" ]; then
+        touch "${installed_packages_dir}/${RUSH_PACKAGE_NAME}"
+    else
+        echo "${version}" > "${installed_packages_dir}/${RUSH_PACKAGE_NAME}"
+    fi
 }
---
+++ _lib/installed_package_version.sh
@@ -0,0 +1,9 @@
+# shellcheck shell=bash
+
+installed_package_version() {
+    local installed_packages_dir
+    installed_packages_dir="$(repo_state_dir)/installed_packages"
+    if [ -f "${installed_packages_dir}/${RUSH_PACKAGE_NAME}" ]; then
+        cat "${installed_packages_dir}/${RUSH_PACKAGE_NAME}"
+    fi
+}
