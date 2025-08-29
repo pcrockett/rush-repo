489a98cd89d69fbc2ccc25ddcb90a82dc0b5f9c3
Philip Crockett <philip.crockett@thermondo.de>
2025-08-29 14:14:32 +0200

    fix(templates): minor usability tweaks

--- _templates/github-release/tasks/populate-info.sh
+++ _templates/github-release/tasks/populate-info.sh
@@ -2,4 +2,6 @@
 set -euo pipefail
 
 REPO="${1?Must specify repository, ex: pcrockett/rush-repo}"
-gh repo view "${REPO}" --json description --template '{{.description}}' > info
+gh repo view "${REPO}" --json description --template '{{.description}}' \
+    | awk '{$1=$1;print}' \
+    > info
