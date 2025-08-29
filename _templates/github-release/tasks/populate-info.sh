c0acb50d8d8ad6d5122525ae6c8a65d78d4d3dd4
Philip Crockett <philip.crockett@thermondo.de>
2025-08-29 12:09:25 +0200

    feat: convert package creation to copier template

---
+++ _templates/github-release/tasks/populate-info.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+REPO="${1?Must specify repository, ex: pcrockett/rush-repo}"
+gh repo view "${REPO}" --json description --template '{{.description}}' > info
