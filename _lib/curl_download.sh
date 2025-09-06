ad9d6338913e5c7a4ad46d04c4c5196030ca9c09
Phil Crockett <contact@philcrockett.com>
2025-09-06 21:00:58 +0200

    fix(github): get auth token from gh if available

--- _lib/curl_download.sh
+++ _lib/curl_download.sh
@@ -13,8 +13,12 @@ curl_download() {
         --fail
     )
 
-    if [ "${GITHUB_TOKEN:-}" != "" ] && [[ "${url}" =~ ^https://api\.github\.com/ ]]; then
-        curl_cmd+=(--header "Authorization: Bearer ${GITHUB_TOKEN}")
+    if [[ "${url}" =~ ^https://api\.github\.com/ ]]; then
+        if [ "${GITHUB_TOKEN:-}" != "" ]; then
+            curl_cmd+=(--header "Authorization: Bearer ${GITHUB_TOKEN}")
+        elif command -v gh &>/dev/null; then
+            curl_cmd+=(--header "Authorization: Bearer $(gh auth token)")
+        fi
     fi
 
     "${curl_cmd[@]}" --location "${url}"
