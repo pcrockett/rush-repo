fed4e4bde26ea25ef0b511584d857cd2d9aee929
Phil Crockett <contact@philcrockett.com>
2025-09-21 09:03:29 +0200

    feat(occasional.d): easier enable / disable

--- occasional.d/occasional.d
+++ occasional.d/occasional.d
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Execute scripts in occasional.d directories
-#
-# Usage: occasional.d [minute|hour|day|week|month]
-#
-# Example usage:
-#
-#     # execute scripts in ~/.config/occasional.d/hour
-#     occasional.d hour
-#
-#     # execute global scripts in /etc/occasional.d/day
-#     sudo occasional.d day
-#
+usage() {
+  cat <<'EOF'
+Execute scripts in occasional.d directories
 
-CLI_ARGS=("$@")
+Usage: occasional.d TIME_INTERVAL [flags...]
+
+Arguments:
+
+    TIME_INTERVAL: minute|hour|day|week|month
+
+Flags:
+
+    --enable: Activate the systemd timer for the given interval
+    --disable: Deactivate the systemd timer for the given interval
+
+Example usage:
+
+    # execute scripts in ~/.config/occasional.d/hour
+    occasional.d hour
+
+    # execute global scripts in /etc/occasional.d/day
+    sudo occasional.d day
+EOF
+}
 
 panic() {
   echo "FATAL: $*" >&2
@@ -22,7 +32,37 @@ panic() {
 }
 
 init() {
-  test "${#CLI_ARGS[@]}" -eq 1 || panic "expecting exactly one argument: minute, hour, day, week, or month"
+  TIME_INTERVAL="${1:-}"
+  shift 1
+
+  case "${TIME_INTERVAL}" in
+    minute|hour|day|week|month)
+      true  # input looks good, carry on
+      ;;
+    *)
+      echo "ERROR: Must specify one of minute, hour, day, week, or month as first argument"
+      usage
+      exit 1
+      ;;
+  esac
+
+  OPERATION="run"
+  while [ "${#@}" -gt 0 ]; do
+    case "${1}" in
+      --enable)
+        OPERATION="enable"
+        ;;
+      --disable)
+        OPERATION="disable"
+        ;;
+      *)
+        echo "ERROR: Unrecognized argument: ${1}"
+        usage
+        exit 1
+        ;;
+    esac
+    shift 1
+  done
 
   if [ "$(id --user)" -eq 0 ]; then
     cd / || panic "unable to cd to /"
@@ -34,42 +74,56 @@ init() {
 
   OCCASIONAL_CONFIG_DIR="${XDG_CONFIG_HOME}/occasional.d"
 
-  TIME_INTERVAL="${CLI_ARGS[0]:-}"
-  case "${TIME_INTERVAL}" in
-    minute|hour|day|week|month)
-      true  # input looks good, carry on
-      ;;
-    "")
-      panic "must specify one of minute, hour, day, week, or month as first argument"
-      ;;
-    *)
-      panic "unrecognized argument: ${TIME_INTERVAL}"
-      ;;
-  esac
+  if [ "$(id --user)" -eq 0 ]; then
+    SYSTEMCTL_CMD=(systemctl)
+  else
+    SYSTEMCTL_CMD=(systemctl --user)
+  fi
 }
 
-main() {
-  init
-  exit_code=0
-  script_dir="${OCCASIONAL_CONFIG_DIR}/${TIME_INTERVAL}"
+# shellcheck disable=SC2329  # function invoked indirectly
+operation:enable() {
+  "${SYSTEMCTL_CMD[@]}" enable --now "occasional.d-${TIME_INTERVAL}.timer"
+  "${SYSTEMCTL_CMD[@]}" enable --now "occasional.d-${TIME_INTERVAL}.service"
+}
 
-  (
-    umask u=rwx,g=,o=  # restrict permissions so it's safe to hard-code secrets in scripts
-    mkdir --parent "${script_dir}"
-  )
-  test -d "${script_dir}" || panic "not a directory: ${script_dir}"
+# shellcheck disable=SC2329  # function invoked indirectly
+operation:disable() {
+  "${SYSTEMCTL_CMD[@]}" disable --now "occasional.d-${TIME_INTERVAL}.timer"
+  "${SYSTEMCTL_CMD[@]}" disable --now "occasional.d-${TIME_INTERVAL}.service"
+}
+
+# shellcheck disable=SC2329  # function invoked indirectly
+operation:run() {
+  exit_code=0
   while read -r script; do
-    echo "starting: ${script}"
-    if "${script}"; then
-      echo "exited with code $?: ${script}"
+    echo "Starting: ${script}"
+    if "${SCRIPT_DIR}/${script}"; then
+      echo "SUCCESS: exited with code $?: ${script}"
     else
       echo "ERROR: exited with code $?: ${script}"
       exit_code=1
     fi
-  done < <(find "${script_dir}" -maxdepth 1 -executable -type f)
-
-  echo "finished executing scripts."
-  exit ${exit_code}
+  done < <(
+    find "${SCRIPT_DIR}" -maxdepth 1 -print0 -executable -type f \
+      | xargs -0 -L 1 basename \
+      | sort
+  )
+  echo "Finished executing scripts."
+  return "${exit_code}"
 }
 
-main
+main() {
+  init "$@"
+  SCRIPT_DIR="${OCCASIONAL_CONFIG_DIR}/${TIME_INTERVAL}"
+
+  (
+    umask u=rwx,g=,o=  # restrict permissions so it's safe to hard-code secrets in scripts
+    mkdir --parent "${SCRIPT_DIR}"
+  )
+  test -d "${SCRIPT_DIR}" || panic "Not a directory: ${SCRIPT_DIR}"
+  "operation:${OPERATION}"
+  exit $?
+}
+
+main "$@"
