52881a0fd3ff009791f776170baf63cc6667e2d3
Phil Crockett <contact@philcrockett.com>
2025-10-24 09:19:37 +0200

    chore: renamed tofurl to yolodl

--- _lib/download_artifact.sh
+++ _lib/download_artifact.sh
@@ -3,8 +3,8 @@
 download_artifact() {
   local url="${1:?Expected URL as first parameter}"
   local dest="${2:?Expected destination path as second parameter}"
-  if command -v tofurl &>/dev/null; then
-    tofurl "${url}" "${dest}" --quiet
+  if command -v yolodl &>/dev/null; then
+    yolodl "${url}" "${dest}" --quiet
   else
     curl_download "${url}" >"${dest}"
   fi
